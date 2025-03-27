b66635f8e62d575687be730164436a6268756604
Saate123 <saateechindo@gmail.com>
2025-03-27 16:22:39 +0100

    second commit

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -5,7 +5,7 @@ import { FaArrowRight } from "react-icons/fa";
 
 const HeroSection = () => {
   return (
-    <section className="py-16 px-6 overflow-hidden">
+    <section className="py-25 px-6 overflow-hidden">
       {" "}
       {/* Prevent horizontal scroll */}
       <div className="max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-12 items-center">
